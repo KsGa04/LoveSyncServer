fb4502d95bb8e2055f0cad49c25e5d2f4dd20d12
Ksenia <kseniagaranceva@gmail.com>
2025-04-11 12:52:08 +0300

    Корректировка program

--- Program.cs
+++ Program.cs
@@ -24,15 +24,13 @@ namespace LoveSyncServer
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
+            builder.WebHost.UseUrls("http://0.0.0.0:8080");
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            
 
             app.UseHttpsRedirection();
 
